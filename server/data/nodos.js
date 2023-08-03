45e945b5af70e06ff8e754f6a76051c3a8697f3d
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2023-08-03 15:01:45 -0500

    chore: add sample data for nodes and routes

---
+++ server/data/nodos.js
@@ -0,0 +1,12 @@
+export default nodos = {
+  nodo1: {
+    x: 0,
+    y: 0,
+    piso: 1,
+  },
+  nodo2: {
+    x: 1,
+    y: 1,
+    piso: 1,
+  },
+};
---
+++ server/data/rutas.js
@@ -0,0 +1,8 @@
+export default rutas = {
+  nodo1: {
+    nodo2: getDistancia(nodos.nodo1, nodos.nodo2),
+  },
+  nodo2: {
+    nodo1: getDistancia(nodos.nodo2, nodos.nodo1),
+  },
+};
